2ee54a0de9c03107317eda25e8ac23ff77d39a68
Hargun Singh Sahni <hargunsinghsahni@gmail.com>
2020-07-22 12:13:16 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api');
+mongoose.connect("mongodb+srv://SINGHHR:hello123@cluster0-ucnr1.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true});
 
 mongoose.Promise = Promise;
 
