cd29a908c01c6ee602409530bb893903f703415b
Hargun Singh Sahni <hargunsinghsahni@gmail.com>
2021-05-30 12:20:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ var todoRoutes = require("./routes/todos");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');    // Request headers you wish to allow
+  // Pass to next layer of middleware
+  next();
+});
 
 app.get('/', function(req, res){
     res.send("From the index.js file");
